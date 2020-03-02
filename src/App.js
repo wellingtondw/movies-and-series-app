a54f715f0e1d219544719c7a7345eb86a54e1d92
wellington <wellingtonslpbi@hotmail.com>
2020-03-01 22:23:20 -0300

    add reactotron to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+if (__DEV__) {
+  import('../ReactotronConfig').then(() =>
+    console.log('Reactotron Configured')
+  );
+}
+
 const App = () => {
   return (
     <View>
